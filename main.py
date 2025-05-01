e48b871bac7bf70e58573175663bc1d9652dd94b
slavyano4kaa <104259254+slavyano4kaa@users.noreply.github.com>
2025-05-01 19:17:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,43 @@
-import vk_api
-import pypresence
-import configparser
 import time
-from chardet.universaldetector import UniversalDetector
+import vk_api
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
 
-print("Программа инициализируется...")
+########################
+app_token = 'yourapptoken' # токен из vk.com/apps?act=manage
+user_id = 'yourvkusername' # id страницы или никнейм
+defaultabout = "youraboutme" # дефолтное описание в телеграме если ничего не играет
+api_id = 000000000 # api_id из my.telegram.org
+api_hash = 'yourapihash' # api_hash из my.telegram.org
+########################
 
-config = configparser.ConfigParser()
-detector = UniversalDetector()
-with open('config.ini', 'rb') as fh:
-    for line in fh:
-        detector.feed(line)
-        if detector.done:
-            break
-    detector.close()
-config.read("config.ini", encoding=detector.result["encoding"])
-app_id = '543726720289734656'
-
-
-def run():
-    try:
-        presence = pypresence.Presence(app_id)
-        presence.connect()
-        vk_session = vk_api.VkApi(token=config['VK']['app_token'])
-        vk = vk_session.get_api()
-
-        print("Приложение было проинициализировано. Запуск через 5 секунд.")
-        time.sleep(5)
-        while True:
-            large_image = "vk"
-            activity = {
-                "large_image": large_image
-            }
-            res = vk.users.get(user_ids=config['VK']['id'], fields="status")[0]
-            print(res)
-
+client = TelegramClient('session', api_id, api_hash)
+async def main():
+    await client.start()
+    user = await client.get_me()
+    print(f"Telegram: {user.first_name} ({user.username})")
+    vk_session = vk_api.VkApi(token=app_token)
+    vk = vk_session.get_api()
+    last_status = None
+    
+    while True:
+        try:
+            res = vk.users.get(user_ids=user_id, fields="status")[0]
             if "status_audio" not in res:
-                state = "Музыка не воспроизводится"
-                if "details" in activity:
-                    activity.pop("details")
-
-                large_image = 'vk'
-                activity.update({'state': state, 'large_image': large_image})
+                status = defaultabout
             else:
-                curr_music = res['status_audio']
+                currentmusic = res["status_audio"]
+                artist = currentmusic["artist"]
+                title = currentmusic["title"]
+                status = f"🎧 Слушает {artist} - {title} | VK Music"
+            if status != last_status:
+                await client(UpdateProfileRequest(about=status))
+                print("Установлено: ", status)
+                last_status = status
+        except Exception as error:
+            print("Error:", error)
+        time.sleep(5)
 
-                state = f"Автор - {curr_music['artist']}"
-                details = f"Трек - {curr_music['title']}"
-                if 'album' in curr_music and 'thumb' in curr_music['album']:
-                    large_image = curr_music["album"]["thumb"]["photo_300"]
-
-                activity.update(
-                    {'state': state, 'details': details,
-                     'large_image': large_image})
-
-            presence.update(**activity)
-            time.sleep(15)
-    except OSError:
-        print("fuck. Restarting.")
-        run()
-
-
-if __name__ == "__main__":
-    run()
+with client:
+    client.loop.run_until_complete(main())
